c59f24800ed97486ec2765179d3d9e45e16facc1
arsatija <arkinsatija@gmail.com>
2025-02-25 10:57:14 -0500

    Corrected index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,11 @@
-// import { drizzle } from "drizzle-orm/neon-serverless";
-// import { neon, Pool } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/neon-serverless";
+import { neon, Pool } from "@neondatabase/serverless";
+// import { drizzle } from "drizzle-orm/node-postgres";
+// import { Pool } from "pg";
 import { config } from "dotenv";
 import * as schema from "./schema";
 
-config({ path: ".env.local" }); // or .env.local
+config({ path: ".env.prod" }); // or .env.local
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-// const sql = neon(process.env.DATABASE_URL!);
 export const db = drizzle(pool, { schema });
