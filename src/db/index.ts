b826f04d0777c23f8d0493623d321cddd46043ad
Rav <64511217+arsatija@users.noreply.github.com>
2025-10-25 22:10:13 -0400

    Remove prod and add local check for .env

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,11 +4,14 @@ import WebSocket from "ws";
 import { config } from "dotenv";
 import * as schema from "./schema";
 
-config({ path: ".env.prod", override: true });
 
 if (process.env.NODE_ENV === "production") {
+    config({ path: ".env.prod", override: true });
+
     neonConfig.webSocketConstructor = WebSocket;
     neonConfig.poolQueryViaFetch = true;
+} else if (process.env.NODE_ENV === "development") {
+    config({ path: ".env.local", override: true });
 }
 
 const pool = new Pool({ connectionString: process.env.DATABASE_URL });
